8b9abb3908dc3dfc7602bd279df2a6868d2b5254
Dario <kdhidalgoa@hotmail.com>
2022-11-04 23:30:28 -0500

    Probado servidor con uvucorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI as fp
+from fastapi.responses import HTMLResponse as web
+
+#fastapi permite crear estructura para el servidor
+#response html permite definir en formato HTML una pagina
+
+#Para ejecutar se usa el comando: uvicorn main:app --reload
+
+app = fp()
+
+@app.get('/')
+def getlist():
+    return[1,2,3]
+
+@app.get('/contact', response_class=web)
+def getlist():
+    return '''
+    <h1> Hola perro </h1>
+    <p> Primer parrafo </p>
+'''
 
 def run():
     store.get_cat()
